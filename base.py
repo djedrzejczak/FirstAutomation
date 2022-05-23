9b0198bc2bf836b4a93dd9f6bcc0037064f55570
djedrzejczak <35876509+djedrzejczak@users.noreply.github.com>
2022-05-23 20:00:18 +0200

    first commit

--- base.py
+++ base.py
@@ -1,11 +1,11 @@
 class BaseTest:
-        @staticmethod
-        def first_method():
-            a = 1
-            b = 2
-            if (a+b) == 4:
-                return False
-            elif (a+b) == 3:
-                return True
+    @staticmethod
+    def first_method():
+        a = 1
+        b = 2
+        if (a + b) == 4:
+            print('False')
+        elif (a + b) == 3:
+            print('True')
 
-
+    first_method()
